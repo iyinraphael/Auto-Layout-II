a6614e986bc3a13a18331c859a9af2a5e5597db7
iyinraphael <iyinraphael@gmail.com>
2018-11-26 10:43:23 -0800

    adjusted leading and top alignment of a view

---
+++ Auto Layout II/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout II
+//
+//  Created by Iyin Raphael on 11/26/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
